4f37170230091980921bfea83a05d935ca7c096f
jakeogh <github.com@v6y.net>
2025-09-24 14:07:27 -0700

    auto-commit

--- proxytool/proxytool.py
+++ proxytool/proxytool.py
@@ -2,27 +2,11 @@
 # -*- coding: utf8 -*-
 # tab-width:4
 
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=missing-module-docstring        # [C0114]
-# pylint: disable=fixme                           # [W0511] todo is encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 
 from __future__ import annotations
 
 import os
+from pathlib import Path
 from signal import SIG_DFL
 from signal import SIGPIPE
 from signal import signal
@@ -35,17 +19,14 @@ from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tv
 from globalverbose import gvd
-from pathtool import read_file_bytes
 
 signal(SIGPIPE, SIG_DFL)
 
 
-def construct_proxy_dict(
-    verbose: bool | int | float = False,
-):
+def construct_proxy_dict():
     try:
         proxy_config = (
-            read_file_bytes("/etc/portage/proxy.conf").decode("utf8").split("\n")
+            Path("/etc/portage/proxy.conf").read_bytes().decode("utf8").split("\n")
         )
     except FileNotFoundError as e:
         icp(e)
@@ -85,7 +66,7 @@ def cli(
     ctx,
     verbose_inf: bool,
     dict_output: bool,
-    verbose: bool | int | float = False,
+    verbose: bool = False,
 ) -> None:
     tty, verbose = tv(
         ctx=ctx,
@@ -108,7 +89,7 @@ def _add_proxy_to_enviroment(
     ctx,
     verbose_inf: bool,
     dict_output: bool,
-    verbose: bool | int | float = False,
+    verbose: bool = False,
 ) -> None:
     tty, verbose = tv(
         ctx=ctx,
